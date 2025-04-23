6d54634ce8ada999698228efc9aa1d361a86fb3b
Emma_D <91540265+EmmaDahlqvist@users.noreply.github.com>
2025-04-23 14:18:57 +0200

    changed search functionality
    tog bort så att geartype o sånt inte gör något utslag, även så att bara hela ord kan matcha i ruleText

--- catch-you-later/src/backend/searchHandler.ts
+++ catch-you-later/src/backend/searchHandler.ts
@@ -21,17 +21,21 @@ export function setupSearchBar(
       return;
     }
 
-    const filterReg = regulations.filter(regulation =>
-      regulation.species.some(specie =>
-        specie.toLowerCase().includes(query)
-      ) ||
-      regulation.gear.toLowerCase().includes(query) ||
-      regulation.location.some(location =>
-        location.name.toLowerCase().includes(query)
-      ) ||
-      regulation.text.toLowerCase().includes(query) ||
-      regulation.type.toLowerCase().includes(query) ||
-      regulation.targetGroup.toLowerCase().includes(query)
+    const filterReg = regulations.filter(regulation => {
+      const ruleTextWords : string[] = regulation.text.toLocaleLowerCase().match(/\p{L}+/gu) || [];
+      return (
+        regulation.species.some(specie =>
+          specie.toLowerCase().includes(query)
+        ) ||
+        regulation.location.some(location =>
+          location.name.toLowerCase().includes(query)
+        ) ||
+        ruleTextWords.includes(query) || 
+        regulation.type.toLowerCase().includes(query) 
+      ); 
+    }
+      
+      
     );
 
     console.log('Filtered regulations:', filterReg);
